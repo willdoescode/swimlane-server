ce688b07e49e35b66bee6c15265518d77c1e4b3a
Will Lane <williamlane923@gmail.com>
2021-01-02 15:30:06 -0800

    removed id from item model

--- handlers/itemModels.go
+++ handlers/itemModels.go
@@ -3,11 +3,10 @@ package handlers
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Item struct {
-	Collection string             `json:"collection,omitempty"`
-	Id         primitive.ObjectID `json:"id"`
-	Title      string             `json:"title,omitempty"`
-	Details    string             `json:"details,omitempty"`
-	Assignee   string             `json:"assignee,omitempty"`
+	Collection string `json:"collection,omitempty"`
+	Title      string `json:"title,omitempty"`
+	Details    string `json:"details,omitempty"`
+	Assignee   string `json:"assignee,omitempty"`
 }
 
 type ReturnItem struct {
