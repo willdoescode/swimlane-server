e4116f238d571063392bbb0b232d4e6dad45b171
Will Lane <williamlane923@gmail.com>
2021-01-03 15:00:12 -0800

    added mux middleware to compress responses and also middleware for logging

--- main.go
+++ main.go
@@ -2,17 +2,20 @@ package main
 
 import (
 	"log"
+	// Need different name then handlers middleware
 	h "main/handlers"
 	"net/http"
+	"os"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
 )
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/create", h.CreateItemHandler).Methods("POST")
-	router.HandleFunc("/move", h.MoveItemHandler).Methods("POST")
-	router.HandleFunc("/{id}", h.GetManyItemsHandler).Methods("GET")
-	log.Fatal(http.ListenAndServe(":8080", cors.Default().Handler(router)))
+	router.Handle("/create", handlers.LoggingHandler(os.Stdout, http.HandlerFunc(h.CreateItemHandler))).Methods("POST")
+	router.Handle("/move", handlers.LoggingHandler(os.Stdout, http.HandlerFunc(h.MoveItemHandler))).Methods("POST")
+	router.Handle("/{id}", handlers.LoggingHandler(os.Stdout, http.HandlerFunc(h.GetManyItemsHandler))).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8080", handlers.CompressHandler(cors.Default().Handler(router))))
 }
